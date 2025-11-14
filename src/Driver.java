e328edc0fef244ed596d2e54f6450da9a6a135f0
Joramm <joramm_delatorre@dlsu.edu.ph>
2025-11-14 18:33:37 +0800

    added nimbus look and feel

--- src/Driver.java
+++ src/Driver.java
@@ -1,9 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
 import java.sql.*;
 //import javax.swing.*;
 //import javax.swing.table.DefaultTableModel;
 //import java.awt.*;
 public class Driver {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        try {
+            UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+            UIManager.put("Button.background", Color.LIGHT_GRAY);
+            UIManager.put("Button.foreground", Color.BLACK);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         AppGUI app = new AppGUI("Health Maintenance Organization");
         AppModel model = new AppModel();
 		
