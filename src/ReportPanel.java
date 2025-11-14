97984f9cf5ef0fe038d2d6c0702a294b224c70c2
Joramm <joramm_delatorre@dlsu.edu.ph>
2025-11-14 11:06:37 +0800

    updated report panel and transaction panel

--- src/ReportPanel.java
+++ src/ReportPanel.java
@@ -1,5 +1,5 @@
-/*
 import javax.swing.*;
+import java.awt.*;
 
 public class ReportPanel extends BasePanel {
     public ReportPanel() {
@@ -10,8 +10,38 @@ public class ReportPanel extends BasePanel {
         options[1] = new JButton("Health Provider Utilization Report");
         options[2] = new JButton("Policy Utilization Report");
         options[3] = new JButton("Illness Trend Report");
+
+        for (int i = 0; i < options.length; i++) {
+            leftPanel.add(options[i]);
+            if (i < options.length - 1) {
+                leftPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+            }
+        }
+
+        updateComponent();
     }
 
+    @Override
+    protected void updateComponent() {
+        if (options != null) {
+            int leftPanelWidth = (int)(AppGUI.screenWidth * 0.3);
+            int hideButtonWidth = 25; // Account for hide button width
+            int availableWidth = leftPanelWidth - hideButtonWidth - 60; // Extra margin for padding
+            int buttonHeight = (int)(AppGUI.screenHeight * 0.08); // Slightly smaller height
+            Dimension buttonSize = new Dimension(availableWidth, buttonHeight);
 
+            Font buttonFont = new Font("Algerian", Font.PLAIN,
+                    Math.max(AppGUI.screenWidth / 100, AppGUI.screenHeight / 60));
+
+            for (JButton option : options) {
+                option.setPreferredSize(buttonSize);
+                option.setMaximumSize(buttonSize);
+                option.setMinimumSize(buttonSize);
+                option.setFont(buttonFont);
+                option.setAlignmentX(Component.CENTER_ALIGNMENT);
+            }
+        }
+        leftPanel.revalidate();
+        leftPanel.repaint();
+    }
 }
-*/
--- src/TransactionPanel.java
+++ src/TransactionPanel.java
@@ -1,5 +1,5 @@
-/*
 import javax.swing.*;
+import java.awt.*;
 
 public class TransactionPanel extends BasePanel {
     public TransactionPanel() {
@@ -13,6 +13,38 @@ public class TransactionPanel extends BasePanel {
         options[4] = new JButton("Payout to Hospital");
         options[5] = new JButton("Payout to Doctor");
         options[6] = new JButton("Request of Letter of Authorization");
+
+        for (int i = 0; i < options.length; i++) {
+            leftPanel.add(options[i]);
+            if (i < options.length - 1) {
+                leftPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+            }
+        }
+
+        updateComponent();
+    }
+
+    @Override
+    protected void updateComponent() {
+        if (options != null) {
+            int leftPanelWidth = (int)(AppGUI.screenWidth * 0.3);
+            int hideButtonWidth = 25; // Account for hide button width
+            int availableWidth = leftPanelWidth - hideButtonWidth - 60; // Extra margin for padding
+            int buttonHeight = (int)(AppGUI.screenHeight * 0.08); // Slightly smaller height
+            Dimension buttonSize = new Dimension(availableWidth, buttonHeight);
+
+            Font buttonFont = new Font("Algerian", Font.PLAIN,
+                    Math.max(AppGUI.screenWidth / 100, AppGUI.screenHeight / 60));
+
+            for (JButton option : options) {
+                option.setPreferredSize(buttonSize);
+                option.setMaximumSize(buttonSize);
+                option.setMinimumSize(buttonSize);
+                option.setFont(buttonFont);
+                option.setAlignmentX(Component.CENTER_ALIGNMENT);
+            }
+        }
+        leftPanel.revalidate();
+        leftPanel.repaint();
     }
 }
-*/
