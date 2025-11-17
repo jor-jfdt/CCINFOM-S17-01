d62aa8dbcf8bbb3f17df644720bf27d4c167711c
deppy0 <dependant1b@gmail.com>
2025-11-17 10:42:56 +0800

    Added necessary libs

--- src/start_app.bat
+++ src/start_app.bat
@@ -1,6 +1,6 @@
 @echo off
-javac -cp "..\lib\mysql-connector-j-9.5.0.jar;." Driver.java
-java -cp "..\lib\mysql-connector-j-9.5.0.jar;." Driver
+javac -cp "..\lib\mysql-connector-j-9.5.0.jar;..\lib\jcalendar-1.4.jar;." Driver.java
+java -cp "..\lib\mysql-connector-j-9.5.0.jar;..\lib\jcalendar-1.4.jar;." Driver
 :: IMPORTANT TO RESET CLASSPATH AFTER USAGE
 set classpath=
 del *.class /f /q
