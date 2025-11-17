4ae6e37babb0151443abeaf2e2b02fc948b5214a
deppy0 <dependant1b@gmail.com>
2025-11-17 16:03:16 +0800

    Implemented search filter function

--- src/_Driver.java
+++ src/_Driver.java
@@ -41,7 +41,7 @@ public class _Driver {
 		);
 		*/
 		DefaultTableModel dtm = model.makeTableModel(model.getTableEntries("client", "*"));
-		TableRowSorter trs = model.filterOnTableRowSorter(dtm, "19", "birth_date");
+		TableRowSorter<DefaultTableModel> trs = model.filterOnTableRowSorter(dtm, "@!6,7,8 f");
 		model.closeDatabase();
 		JFrame jf = new JFrame("Sample table");
 		JTable jt = new JTable(dtm);
